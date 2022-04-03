56a4fd6c0fac22f9afb8a8c692f5517f5eb7c511
HARIPRASATH S <101514904+Hariprasath19@users.noreply.github.com>
2022-04-03 17:12:28 +0530

    Update EditContact.c

--- 3_Implementation/src/EditContact.c
+++ 3_Implementation/src/EditContact.c
@@ -1,1 +1,59 @@
+#include<Contact_Managment_system.h>
 
+/**
+ * @brief function to modify the Contact
+ * 
+ * 
+ * @return test_values 
+ */
+
+test_values editContact(char name[20],long ph,char address[20],char email[30])
+{
+  system("cls");
+
+        fp=fopen("contact.dll","r");
+        ft=fopen("temp.dat","w");
+
+        fflush(stdin);
+
+        printf("..::Edit contact\n===============================\n\n\t..::Enter the name of contact you wish to edit:");
+        scanf("%[^\n]",name);
+
+        while(fread(&list,sizeof(list),1,fp)==1)
+        {
+            if(stricmp(name,list.name)!=0)
+                fwrite(&list,sizeof(list),1,ft);
+        }
+        fflush(stdin);
+
+        printf("\n\n..::Editing '%s'\n\n",name);
+        printf("..::Name:");
+        scanf("%[^\n]",&list.name);
+
+        fflush(stdin);
+
+        printf("..::Phone.No:");
+        scanf("%ld",&list.ph);
+
+        fflush(stdin);
+
+        printf("..::address:");
+        scanf("%[^\n]",&list.address);
+
+        fflush(stdin);
+
+        printf("..::email ID:");
+        gets(list.email);
+
+        printf("\n");
+
+        fwrite(&list,sizeof(list),1,ft);
+
+        fclose(fp);
+        fclose(ft);
+
+        remove("contact.dll");
+        rename("temp.dat","contact.dll");
+        printf("Contact Modified Successfully");
+        break;
+}
